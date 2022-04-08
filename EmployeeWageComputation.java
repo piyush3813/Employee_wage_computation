558161e1098d7c8b54ed24d69f73f41c2fc725c3
unknown <piyushmishra.p@gmail.com>
2022-04-09 00:23:35 +0530

    employee wage calculation per day for 8 hrs at 20 rupees

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -10,12 +10,17 @@ public class EmployeeWageComputation {
         logger.info("Welcome To Employee Wage Computation ");
 
         final int FULL_TIME = 1;
+        final int WAGE_PER_HRS = 20;
+        int working_hrs = 0;
         int emp_typ=(int) ((Math.random()*100)%2);
         if (emp_typ==FULL_TIME){
             logger.info("the employee is Present");
+            working_hrs=8;
         }
         else{
             logger.info("the employee is Absent");
         }
+        int wage = working_hrs * WAGE_PER_HRS;
+        logger.info("employee wage per day is - "+wage);
     }
 }
